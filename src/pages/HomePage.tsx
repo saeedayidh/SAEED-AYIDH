5c61dc328698995a0d4c7cccb541786ba4d5b115
سعيد بن عايد <157489377+saeedayidh@users.noreply.github.com>
2026-09-05 14:05:31 +0300

    refactor: apply requested homepage section order

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,21 +10,18 @@ import { HelpAndBlogSection } from '../components/HelpAndBlogSection';
 import { ActionCardsSection } from '../components/ActionCardsSection';
 import { FinalContactCta } from '../components/FinalContactCta';
 import { SaeedPlatformSection } from '../components/SaeedPlatformSection';
-
-export const HomePage: React.FC = () => {
-  return (
-    <div className="animate-fade-in">
-      <HeroSection />
-      <ContentFieldsSection />
-      <WorkFieldsSection />
-      <ServicesSection />
-      <NewsSection />
-      <ResourcesSection />
-      <PortfolioSection />
-      <SaeedPlatformSection />
-      <FinalContactCta />
-      <HelpAndBlogSection />
-      <ActionCardsSection />
-    </div>
-  );
-};
+export const HomePage:React.FC=()=> <div className="animate-fade-in">
+<HeroSection/>
+<SaeedPlatformSection ids={['numbers','id','digital-card','press']}/>
+<WorkFieldsSection/>
+<ServicesSection/>
+<PortfolioSection/>
+<SaeedPlatformSection ids={['projects','labs']}/>
+<ContentFieldsSection/>
+<SaeedPlatformSection ids={['play','stories','music','frame','schedule','countdown','ideas','cinema','background-audio','interactive-stories','live-radar','content-filter','interactive-preview','fan-club']}/>
+<ResourcesSection/>
+<SaeedPlatformSection ids={['wallpapers']}/>
+<NewsSection/>
+<SaeedPlatformSection ids={['archive','questions','notify','polls','analytics','performance']}/>
+<FinalContactCta/><HelpAndBlogSection/><ActionCardsSection/>
+</div>;
